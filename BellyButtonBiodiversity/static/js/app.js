7439012b8c6330f0053f92fe831b04815615facc
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-16 17:50:40 -0400

    populate dropdown menu

--- BellyButtonBiodiversity/static/js/app.js
+++ BellyButtonBiodiversity/static/js/app.js
@@ -1,6 +1,23 @@
-const data = samples.json;
-
 // Fetch the JSON data and console log it
-d3.json(data).then(function(sampleData) {
-    console.log(sampleData);
+d3.json("samples.json").then(function(data) {
+    console.log(data);
   });
+
+// Populate dropdown
+function dropdown(){
+    var drop = d3.select("#selDataset")
+    d3.json("samples.json").then((sampledata)=>{
+        var names = sampledata.names;
+        names.forEach((data)=>{
+            drop.append("option")
+            .text(data)
+            .property("value",data);
+        });
+        var sample = names[0];
+        metadata(sample);
+        buildChart(sample);
+    });
+};
+dropdown();
+
+
