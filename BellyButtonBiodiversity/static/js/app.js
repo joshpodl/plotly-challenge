25f8b8e520ddf0360e3e835ce9a6fcc8f81ac54a
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-05-16 18:18:18 -0400

    metadata and charts

--- BellyButtonBiodiversity/static/js/app.js
+++ BellyButtonBiodiversity/static/js/app.js
@@ -15,7 +15,7 @@ function dropdown(){
         });
         var sample = names[0];
         metadata(sample);
-        buildChart(sample);
+        plots(sample);
     });
 };
 dropdown();
@@ -31,8 +31,47 @@ function metadata(sampleID){
         demoDisplay.html("");
         Object.entries(mainData).forEach(([key,value])=>{
             demoDisplay.append("h6").text(`${key} ${value}`);
+        });
     });
-});
 };
 
+// Create bar chart and bubble chart
+function plots(sampleID){
+    d3.json("samples.json").then((sampledata)=>{
+        var samples = sampledata.samples;
+        var dataArray = samples.filter(row=>row.id==sampleID);
+        console.log(dataArray);
+        var sample = dataArray[0];
+        var otu_ids = sample.otu_ids;
+        var sample_values = sample.sample_values;
+        var otu_labels = sample.otu_labels;
 
+        // Bar chart
+        var barData = [{
+            x: sample_values.slice(0,10).reverse(),
+            y: otu_ids.slice(0,10).map(otu_ids=>`OTU${otu_ids}`).reverse(),
+            text: otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h"
+        }];
+        Plotly.newPlot("bar", barData);
+
+        // Bubble chart
+        var bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids},
+            text: otu_labels 
+        }];
+        Plotly.newPlot("bubble", bubbleData);
+    });
+};
+
+// Have metadata and plots appear when an ID is clicked
+function optionChanged(newData){
+    metadata(newData);
+    plots(newData);
+};
